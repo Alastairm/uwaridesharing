d9e659731527d9ee1dd72ee9bb5e8eba70db1ee2
alastairm <alastair.mory@gmail.com>
2016-09-07 19:28:02 +0800

    Added unit testing support

---
+++ Components/Test.jsx
@@ -0,0 +1,12 @@
+import React, { View, Text } from 'react-native';
+
+function Test() {
+  return (
+    <View>
+      <Text>Test</Text>
+      <Text>Class</Text>
+    </View>
+  );
+}
+
+export default Test;
---
+++ test/Test.js
@@ -0,0 +1,18 @@
+
+import {shallow} from 'enzyme';
+import {expect} from 'chai';
+
+import React, { View, Text } from 'react-native';
+import Test from '../Components/Test.jsx';
+
+describe('<Test />', () => {
+  it('it should render 1 view component', () => {
+    const wrapper = shallow(<Test/>);
+    expect(wrapper.find(View)).to.have.length(1);
+  });
+
+  it('it should render 2 text components', () => {
+    const wrapper = shallow(<Test/>);
+    expect(wrapper.find(Text)).to.have.length(2);
+  });
+});
